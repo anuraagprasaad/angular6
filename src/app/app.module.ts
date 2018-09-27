3666c69578ed2bed48268b76f2ec7cd06bce5f1d
anurag prasad <anuraag.prasaad@gmail.com>
2018-09-27 19:29:09 +0800

    First Commit with Register and Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/Forms';
 import { NgModule } from '@angular/core';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppRoutesModule } from './app-routes.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutesModule,
+    FlashMessagesModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase, 'weblog'),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB3SQt3JVNmWg49U-119Bmtgnpfgmer588",
+    authDomain: "awesomeprojects-e4c4f.firebaseapp.com",
+    databaseURL: "https://awesomeprojects-e4c4f.firebaseio.com",
+    projectId: "awesomeprojects-e4c4f",
+    storageBucket: "awesomeprojects-e4c4f.appspot.com",
+    messagingSenderId: "70048656143"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
