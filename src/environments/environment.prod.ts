911facc708c840069a547a204b53840dcb14fc0d
anuraagprasaad <anuraagprasaad@gmail.com>
2018-09-29 16:07:30 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-<<<<<<< HEAD
-    apiKey: "AIzaSyB3SQt3JVNmWg49U-119Bmtgnpfgmer588",
-    authDomain: "awesomeprojects-e4c4f.firebaseapp.com",
-    databaseURL: "https://awesomeprojects-e4c4f.firebaseio.com",
-    projectId: "awesomeprojects-e4c4f",
-    storageBucket: "awesomeprojects-e4c4f.appspot.com",
-    messagingSenderId: "70048656143"
-=======
     apiKey: "",
     authDomain: "",
     databaseURL: "",
